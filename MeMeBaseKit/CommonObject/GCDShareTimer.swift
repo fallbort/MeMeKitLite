41eae119173ddd7f881aae44d91d8a6800571cfb
xfb <exbice@qq.com>
2023-06-01 19:43:40 +0800

    sharedtime增加block回调

--- MeMeBaseKit/CommonObject/GCDShareTimer.swift
+++ MeMeBaseKit/CommonObject/GCDShareTimer.swift
@@ -24,26 +24,50 @@ public class GCDShareTimer {
     public func flash() {  //观察时间变量后，需要flash才能启动定时器
         let hasObservers = timeObser.hasObservers
         self.lock.lock()
+        let hasBlocks = self.timeBlocks.count > 0
         let hasTimer = gcdTimer != nil
+        var needStart = false
+        var needStop = false
+        if hasObservers == true || hasBlocks == true, hasTimer == false {
+            needStart = true
+        }else if hasTimer == true {
+            needStop = true
+        }
         self.lock.unlock()
-        if hasObservers == true,hasTimer == false {
+        if needStart {
             self.queue.async {
                 self.startTimer()
             }
             
-        }else if hasTimer == true {
+        }else if needStop {
             self.queue.async {
                 self.clear()
             }
         }
     }
     
+    //两种方式添加
+    public func addTimeBlock(key:String,block:((_ totalTime:Double,_ perTime:Double)->())?) {
+        if let block = block {
+            self.lock.lock()
+            self.timeBlocks[key] = block
+            self.lock.unlock()
+        }
+    }
+    
+    public func removeTimeBlock(key:String) {
+        self.lock.lock()
+        self.timeBlocks.removeValue(forKey: key)
+        self.lock.unlock()
+    }
+    
     fileprivate func clear() {
         let hasObservers = timeObser.hasObservers
         
         self.lock.lock()
+        let hasBlocks = self.timeBlocks.count > 0
         let hasTimer = gcdTimer != nil
-        if hasObservers == false && hasTimer == true {
+        if hasObservers == false && hasBlocks == false && hasTimer == true {
             self.gcdTimer?.cancel()
             self.gcdTimer = nil
             self.timepassed = 0.0
@@ -52,27 +76,40 @@ public class GCDShareTimer {
     }
     
     fileprivate func startTimer() {
-        let interval:Double = 0.5
+        let interval:Double = 0.25
         let tickBlock:(VoidBlock) = { [weak self] in
             guard let `self` = self else {return}
-            self.timepassed += interval
             let hasObservers = self.timeObser.hasObservers
-            if hasObservers == false {
+            self.lock.lock()
+            self.timepassed += interval
+            let hasBlocks = self.timeBlocks.count > 0
+            let passed = self.timepassed
+            self.lock.unlock()
+            if hasObservers == false && hasBlocks == false {
                 self.queue.async { [weak self] in
                     self?.clear()
                 }
             }else{
                 DispatchQueue.global().async { [weak self] in
                     guard let `self` = self else {return}
-                    self.timeObser.onNext(self.timepassed)
+                    self.timeObser.onNext((passed,interval))
+                    self.lock.lock()
+                    let blocks = self.timeBlocks
+                    self.lock.unlock()
+                    blocks.forEach { (key,value) in
+                        DispatchQueue.global().async {
+                            value(passed,interval)
+                        }
+                    }
                 }
             }
         }
         let hasObservers = timeObser.hasObservers
         
         self.lock.lock()
+        let hasBlocks = self.timeBlocks.count > 0
         let hasTimer = gcdTimer != nil
-        if hasObservers == true && hasTimer == false {
+        if hasObservers == true || hasBlocks == true, hasTimer == false {
             self.timepassed = 0
         }
         self.lock.unlock()
@@ -81,8 +118,9 @@ public class GCDShareTimer {
         var added = false
         let hasObservers2 = timeObser.hasObservers
         self.lock.lock()
+        let hasBlocks2 = self.timeBlocks.count > 0
         let hasTimer2 = gcdTimer != nil
-        if hasObservers2 == true && hasTimer2 == false {
+        if hasObservers2 == true || hasBlocks2 == true, hasTimer2 == false {
             self.gcdTimer = timer
             added = true
         }
@@ -96,7 +134,9 @@ public class GCDShareTimer {
     
     //MARK: <>内部UI变量
     //MARK: <>内部数据变量
-    public var timeObser = BehaviorSubject<Double>(value: 0.0) //返回时间，代表走了多少时间,单位s,首次不一定是0
+    public var timeObser = BehaviorSubject<(total:Double,perTime:Double)>(value: (0.0,0.0)) //返回时间，代表走了多少时间,单位s,首次不一定是0
+    
+    fileprivate var timeBlocks:[String:((_ totalTime:Double,_ perTime:Double)->())] = [:]
     
     
     fileprivate var _queue:DispatchQueue?
