b0b895cf6de0f9473e6a4b53b195140b35325581
xfb <exbice@qq.com>
2023-06-02 14:21:11 +0800

    优化代码

--- MeMeBaseKit/OC_CommonObject/SystemUtil+ext.m
+++ MeMeBaseKit/OC_CommonObject/SystemUtil+ext.m
@@ -10,7 +10,11 @@
 //系统库
 #include <CoreTelephony/CTTelephonyNetworkInfo.h> //添加获取客户端运营商 支持
 #include <CoreTelephony/CTCarrier.h>
+#ifdef SWIFT_INSTALL_OBJC_HEADER
 #import <MeMeKit-Swift.h>
+#else
+#import <MeMeKit/MeMeKit-Swift.h>
+#endif
 
 @implementation SystemUtil (countrycode)
 
