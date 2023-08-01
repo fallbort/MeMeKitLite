edb3390377dc08fa767815b9da725e2fc0810d5c
xfb <exbice@qq.com>
2023-08-01 20:09:14 +0800

    增加extension

--- MeMeBaseKit/Extensions/UIImage+Extenstions.h
+++ MeMeBaseKit/Extensions/UIImage+Extenstions.h
@@ -12,6 +12,8 @@ NS_ASSUME_NONNULL_BEGIN
 @interface UIImage (scale)
 //等比例缩放
 - (UIImage*)scaleToSize:(CGSize)size;
+//图片压缩到指定大小
++(UIImage *)compressImageSize:(UIImage *)image toByte:(NSUInteger)maxLength;
 @end
 
 NS_ASSUME_NONNULL_END
--- MeMeBaseKit/Extensions/UIImage+Extenstions.m
+++ MeMeBaseKit/Extensions/UIImage+Extenstions.m
@@ -49,4 +49,43 @@
     // 返回新的改变大小后的图片
     return scaledImage;
 }
+
+
++(UIImage *)compressImageSize:(UIImage *)image toByte:(NSUInteger)maxLength{
+    CGFloat compression = 1;
+    NSData *data = UIImageJPEGRepresentation(image, compression);
+    if (data.length < maxLength) return image;
+    
+    CGFloat max = 1;
+    CGFloat min = 0;
+    for (int i = 0; i < 6; ++i) {
+        compression = (max + min) / 2;
+        data = UIImageJPEGRepresentation(image, compression);
+        if (data.length < maxLength * 0.9) {
+            min = compression;
+        } else if (data.length > maxLength) {
+            max = compression;
+        } else {
+            break;
+        }
+    }
+    UIImage *resultImage = [UIImage imageWithData:data];
+    if (data.length < maxLength) return resultImage;
+    
+    // Compress by size
+    NSUInteger lastDataLength = 0;
+    while (data.length > maxLength && data.length != lastDataLength) {
+        lastDataLength = data.length;
+        CGFloat ratio = (CGFloat)maxLength / data.length;
+        CGSize size = CGSizeMake((NSUInteger)(resultImage.size.width * sqrtf(ratio)),
+                                 (NSUInteger)(resultImage.size.height * sqrtf(ratio))); // Use NSUInteger to prevent white blank
+        UIGraphicsBeginImageContext(size);
+        [resultImage drawInRect:CGRectMake(0, 0, size.width, size.height)];
+        resultImage = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        data = UIImageJPEGRepresentation(resultImage, compression);
+    }
+    
+    return resultImage;
+}
 @end
